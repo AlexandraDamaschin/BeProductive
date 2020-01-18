855f36810205fc36b6de66797f6d8e4017fe3637
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2020-01-19 01:49:32 +0200

    Refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -144,131 +144,131 @@ class GridListGames extends StatelessWidget {
                 Container(
                     padding: const EdgeInsets.all(8),
                     color: Colors.teal[200],
-                    child: Center(
-                        child: Column(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
                           'Attention',
                           style: Theme.of(context).textTheme.headline,
                         ),
                         Image(
-                            width: 100,
-                            height: 100,
+                            width: 75,
+                            height: 75,
                             image: AssetImage('assets/images/1.png'))
                       ],
-                    ))),
+                    )),
                 Container(
                     padding: const EdgeInsets.all(8),
                     color: Colors.teal[200],
-                    child: Center(
-                        child: Column(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
                           'Cognitive control',
                           style: Theme.of(context).textTheme.headline,
                         ),
                         Image(
-                            width: 100,
-                            height: 100,
+                            width: 75,
+                            height: 75,
                             image: AssetImage('assets/images/2.jfif'))
                       ],
-                    ))),
+                    )),
                 Container(
                     padding: const EdgeInsets.all(8),
                     color: Colors.teal[300],
-                    child: Center(
-                        child: Column(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
                           'Processing speed',
                           style: Theme.of(context).textTheme.headline,
                         ),
                         Image(
-                            width: 100,
-                            height: 100,
+                            width: 75,
+                            height: 75,
                             image: AssetImage('assets/images/3.png'))
                       ],
-                    ))),
+                    )),
                 Container(
                     padding: const EdgeInsets.all(8),
                     color: Colors.teal[400],
-                    child: Center(
-                        child: Column(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
                           'Judgement',
                           style: Theme.of(context).textTheme.headline,
                         ),
                         Image(
-                            width: 100,
-                            height: 100,
+                            width: 75,
+                            height: 75,
                             image: AssetImage('assets/images/4.jfif'))
                       ],
-                    ))),
+                    )),
                 Container(
                     padding: const EdgeInsets.all(8),
                     color: Colors.teal[500],
-                    child: Center(
-                        child: Column(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
                           'Problem Solving',
                           style: Theme.of(context).textTheme.headline,
                         ),
                         Image(
-                            width: 100,
-                            height: 100,
+                            width: 75,
+                            height: 75,
                             image: AssetImage('assets/images/5.png'))
                       ],
-                    ))),
+                    )),
                 Container(
                     padding: const EdgeInsets.all(8),
                     color: Colors.teal[600],
-                    child: Center(
-                        child: Column(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
                           'Continuous attention',
                           style: Theme.of(context).textTheme.headline,
                         ),
                         Image(
-                            width: 100,
-                            height: 100,
+                            width: 75,
+                            height: 75,
                             image: AssetImage('assets/images/6.png'))
                       ],
-                    ))),
+                    )),
                 Container(
                     padding: const EdgeInsets.all(8),
                     color: Colors.teal[700],
-                    child: Center(
-                        child: Column(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
                           'Memory',
                           style: Theme.of(context).textTheme.headline,
                         ),
                         Image(
-                            width: 100,
-                            height: 100,
+                            width: 75,
+                            height: 75,
                             image: AssetImage('assets/images/7.jpg'))
                       ],
-                    ))),
+                    )),
                 Container(
                     padding: const EdgeInsets.all(8),
                     color: Colors.teal[800],
-                    child: Center(
-                        child: Column(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Text(
                           'Comprehension',
                           style: Theme.of(context).textTheme.headline,
                         ),
                         Image(
-                            width: 100,
-                            height: 100,
+                            width: 75,
+                            height: 75,
                             image: AssetImage('assets/images/8.jpg'))
                       ],
-                    ))),
+                    )),
               ],
             )),
       ),
